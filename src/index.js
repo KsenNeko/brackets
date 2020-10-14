42774f36e205797876830f2ceb3e3f9114db1cfa
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2020-10-14 23:00:12 +0400

    feat: added function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   if (str.length % 2 ===1) return false;
+  while (str.length > 0) {
+    let ind1 = -1;
+    for (let i in bracketsConfig)
+    {
+      if (str[0] === bracketsConfig[i][0] && ind1 === -1) {
+        ind1 = i;
+      }
+    }
+    if (ind1 === -1) return false;
+    let ind2 = -1;
+    for (i in str) {
+      if (str[i] === bracketsConfig[ind1][1] && ind2 === -1) 
+      {
+        ind2 = i;
+      }
+    }
+    if(ind2 === -1) return false;
+    if(ind2%2 === 0)
+    return false;
+    str = str.slice(1, ind2) + str.slice(ind2+1);
+  }
+  return true;
 }
